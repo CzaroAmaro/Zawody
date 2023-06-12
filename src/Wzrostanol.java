daa12c67c55e2f1a17dfa9a8550c64277e498a49
sebson02 <112127651+sebson02@users.noreply.github.com>
2023-06-12 20:08:27 +0200

    Update Wzrostanol.java

--- src/Wzrostanol.java
+++ src/Wzrostanol.java
@@ -7,6 +7,9 @@ public class Wzrostanol extends Przedmiot {
     public void opis(){
         System.out.println("Zwiększa wzrost danego zawodnika o 20cm");
     }
+    public void uzyj(Zawodnik a){
+        a.addWzrost(20);
+    }
     public String toString(){
         return "Wzrostanol";
     }
