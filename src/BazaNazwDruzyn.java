c0e5c719ad0a16a3b55c5e00334cb1e3a5d62bee
sebson02 <112127651+sebson02@users.noreply.github.com>
2023-06-12 20:28:40 +0200

    Create BazaNazwDruzyn.java

---
+++ src/BazaNazwDruzyn.java
@@ -0,0 +1,49 @@
+package src;
+public class BazaNazwDruzyn {
+    public static String wybierz(int a) {
+        switch (a) {
+            case 1:
+                return "Cegła team";
+            case 2:
+                return "Białostocka komunikacja miejska";
+            case 3:
+                return "Dzikie kasztany";
+            case 4:
+                return "Dżungla team";
+            case 5:
+                return "Bezbeki";
+            case 6:
+                return "PROGRAMIŚCIIIII";
+            case 7:
+                return "Weeb team";
+            case 8:
+                return "Wuefiści team";
+            case 9:
+                return "Paśnik team";
+            case 10:
+                return "Zdana sesja";
+            case 11:
+                return "Krzysztof Tyniec fanbois";
+            case 12:
+                return "Ninjów garść";
+            case 13:
+                return "Franklin i przyjaciele";
+            case 14:
+                return "Latający cyrk monty pajtona";
+            case 15:
+                return "Luzik arbuzik";
+            case 16:
+                return "Kiść bananów team";
+            case 17:
+                return "Słoik team";
+            case 18:
+                return "You shall not pass team";
+            case 19:
+                return "Dzikie szony";
+            case 20:
+                return "Gąszcz";
+            default:
+                return "Gostyń team";
+        }
+    }
+}
