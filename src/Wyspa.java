10405239cbaceca7275e368474ea896b3dca3232
sebson02 <112127651+sebson02@users.noreply.github.com>
2023-06-12 20:20:17 +0200

    Update Wyspa.java

--- src/Wyspa.java
+++ src/Wyspa.java
@@ -1,4 +1,21 @@
 package src;
 
 public class Wyspa {
+  private static ArrayList<Druzyna> listaDruzyn = new ArrayList<>();
+    private int iloscDruzyn = 0;
+    public void dodajDruzyne(Druzyna a){
+        listaDruzyn.add(a);
+        iloscDruzyn += 1;
+    }
+    public Druzyna gerDruzyna(int a){
+        return listaDruzyn.get(a);
+    }
+    public int getIlosc(){
+        return iloscDruzyn;
+    }
+    public void wyswietlDruzyny(){
+        for (int i=0; i<iloscDruzyn; i++){
+            listaDruzyn.get(i).toString();
+        }
+    }
 }
