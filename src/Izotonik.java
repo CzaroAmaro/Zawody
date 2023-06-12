c1be059aff351fabf117df0716af4a5ce73a52d0
sebson02 <112127651+sebson02@users.noreply.github.com>
2023-06-12 19:45:35 +0200

    Update Izotonik.java

--- src/Izotonik.java
+++ src/Izotonik.java
@@ -6,7 +6,6 @@ public class Izotonik extends Przedmiot {
     }
     public void opis(){
         System.out.println("Zwiększa szybkość i refleks danego zawodnika o 10");
-        System.out.println("Czas działania: jeden mecz");
     }
     public String toString(){
         return "Napój izotoniczny";
