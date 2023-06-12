338439c1db39894833d37db3fe83b6b3e895d2c8
sebson02 <112127651+sebson02@users.noreply.github.com>
2023-06-12 19:57:50 +0200

    Update Piwo.java

--- src/Piwo.java
+++ src/Piwo.java
@@ -6,8 +6,9 @@ public class Piwo extends Przedmiot{
     public void opis(){
         System.out.println("Zwiększa siłę o 10, ale zmniejsza celność o 20");
     }
-    public void uzyj(){
-
+   public void uzyj(Zawodnik a){
+        a.addSila(10);
+        a.addCelnosc(-20);
     }
     public String toString(){
         return "Piwo";
