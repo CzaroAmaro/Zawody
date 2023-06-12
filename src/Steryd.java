eb5f2a098131a9243bdc2b8446b8b739361786fb
sebson02 <112127651+sebson02@users.noreply.github.com>
2023-06-12 20:07:21 +0200

    Update Steryd.java

--- src/Steryd.java
+++ src/Steryd.java
@@ -7,6 +7,11 @@ public class Steryd extends Przedmiot {
     public void opis(){
         System.out.println("Zwiększa siłe zawodnika o 10, zwiększa masę o 20kg, ale obniża szybkość oraz refleks o 10");
     }
+    public void uzyj(Zawodnik a){
+        a.addSila(10);
+        a.addWaga(20);
+        a.addRefleks(-10);
+    }
     public String toString(){
         return "Steryd";
     }
