2dc1feb199c4c8fbb485d0abc34de8ada57ae87f
sebson02 <112127651+sebson02@users.noreply.github.com>
2023-06-12 19:46:02 +0200

    Update Karnetnasilke.java

--- src/Karnetnasilke.java
+++ src/Karnetnasilke.java
@@ -4,7 +4,6 @@ public class Karnetnasilke extends Przedmiot{
     }
     public void opis(){
         System.out.println("Zwiększa siłę, szybkość oraz refleks danego zawodnika o 10");
-        System.out.println("Czas działania: permanentne");
     }
     public String toString(){
         return "Karnet na siłownie";
