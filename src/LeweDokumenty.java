3bd26682cbae708f9ce9077e4eaa0388f450a6d5
sebson02 <112127651+sebson02@users.noreply.github.com>
2023-06-16 23:19:19 +0200

    Update LeweDokumenty.java

--- src/LeweDokumenty.java
+++ src/LeweDokumenty.java
@@ -8,11 +8,13 @@ public class LeweDokumenty extends Przedmiot {
     }
     public void opis(){
         System.out.println("Pozwala na zmianę imienia danego zawodnika");
+        System.out.println("Czas działania: permanentne");
     }
     public void uzyj(Zawodnik a){
         Scanner x = new Scanner(System.in);
         System.out.println("Podaj nowe imię:");
-        a.zmienImie(x.next());
+        String noweimie = x.next();
+        a.zmienImie(noweimie);
     }
     public String toString(){
         return "Lewe dokumenty";
