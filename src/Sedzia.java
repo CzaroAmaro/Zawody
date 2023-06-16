813ce988b1f6d4c2181a2fa941d8322391f56416
CzaroAmaro <babik.czarek@gmail.com>
2023-06-16 23:56:07 +0200

    16.06

--- src/Sedzia.java
+++ src/Sedzia.java
@@ -1,19 +1,32 @@
 package src;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Sedzia implements Serializable{
     private String nazwisko;
+    List<Sedzia> listaSedziow = new ArrayList<>();
 
     public Sedzia(String nazwisko){
         this.nazwisko = nazwisko;
     }
+    public String getNazwisko(){
+        return nazwisko;
+    }
 
-    public static void Dodajsedziego(String nazwisko, List<Sedzia> listaSedziow){
+    public static void dodajSedziego(String nazwisko, List<Sedzia> listaSedziow){
        Sedzia nowySedzia = new Sedzia(nazwisko);
        listaSedziow.add(nowySedzia);
         System.out.println("Dodano sedziego:" + nazwisko);
-
-
+    }
+    public static void usunSedziego(String nazwisko, List<Sedzia> listaSedziow){
+        listaSedziow.remove(nazwisko);
+    }
+    public static void wyswietlSedziow(List<Sedzia> listaSedziow){
+        int index = 1;
+        for(Sedzia sedzia : listaSedziow) {
+            System.out.println(index + " " + sedzia.getNazwisko());
+            index ++;
+        }
     }
 }
