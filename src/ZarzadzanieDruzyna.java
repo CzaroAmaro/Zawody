9d1fef9a796402fdba7d1cf0c4ec52e25892a16e
sebson02 <112127651+sebson02@users.noreply.github.com>
2023-06-17 20:56:08 +0200

    Update ZarzadzanieDruzyna.java

--- src/ZarzadzanieDruzyna.java
+++ src/ZarzadzanieDruzyna.java
@@ -171,6 +171,9 @@ public class ZarzadzanieDruzyna {
                 }
                 else System.out.println("Za mało pieniędzy!");
             }
+            if (d != 0 && d != 1 && d != 2 && d != 4 && d != 5 && d != 6){
+                System.out.println("Nie ma takiego przedmiotu");
+            }
         }while(d!=0);
     }
 }
